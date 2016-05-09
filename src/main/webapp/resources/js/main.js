0409f75c28dc78bf6ee63a700453a8500f1486dd
Ktulhu <geekhub@mail.ru>
2016-05-09 20:14:17 +0300

    temp1

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -1,18 +1,24 @@
-$(document).ready(function() {
-    $("#button").click(createUser)
-});
+//$(document).ready(function() {
+//    $("#DeleteButton").click(createUser)
+//});
 
-function createUser() {
-    var firstName = $("#firstName").val();
-    var lastName = $("#lastName").val();
-    var email = $("#email").val();
+function DeletePost(id) {
+    //var firstName = $("#firstName").val();
+    //var lastName = $("#lastName").val();
+    //var email = $("#email").val();
     //var firstName = document.getElemtentById("fistname").value;
-    $.post("/createUser", {
-        fName: firstName,
-        lName: lastName,
-        e: email
-    }, function(data) {
-        var json = JSON.parse(data);
-        alert("Status - " + json.status);
-    })
+    $.post("/deletePost", {
+            //fName: firstName,
+            //lName: lastName,
+            //e: email
+            id: id
+        }
+        , function(data) {
+            //var json = JSON.parse(data);
+
+            window.location.reload();
+            //  alert(data);
+            //  $("#acontent").html(data);
+        }
+    )
 }
